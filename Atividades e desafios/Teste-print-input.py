47fe8829f1af4e3c9576b7e463ac3cf1a7968382
João Paulo Lunkes <joaopaulolunkes@gmail.com>
2026-07-22 18:52:06 -0300

    app.py incomplete version 1 and some print/input tests created

---
+++ Atividades e desafios/Teste-print-input.py
@@ -0,0 +1,9 @@
+print ('\nPython na Escola de Programação Alura.\n')
+
+print('A','L','U','R','A',sep='\n')
+
+nome_do_caboclo = input ('\nQual o seu nome?\n')
+
+idade_do_fulano = input ('\nE qual a sua idade, em anos?\n')
+
+print (f'\nMuito prazer, {nome_do_caboclo}! Bora viver a vida! Só se tem {idade_do_fulano} uma vez!')
--- Sabor-Express/app.py
+++ Sabor-Express/app.py
@@ -1,10 +1,35 @@
-print('\nSabor Express\n')
+print('\n𝑺𝒂𝒃𝒐𝒓 𝑬𝒙𝒑𝒓𝒆𝒔𝒔\n')
 
 print ('1. Cadastrar restaurante')
 print ('2. Listar restaurantes')
 print ('3. Ativar restaurante')
 print ('4. Sair\n')
 
-opcao_escolhida = input ('Escolha uma opção: ')
-print ('Você escolheu a opção', opcao_escolhida)
+#opcao_escolhida = input ('Escolha uma opção: ') Não funciona aqui, porque a entrada, por padrão, é uma string. 
+# Duas opções:
 
+#Após o input que dá em string, faz uma ova variável que transforma o string em int:
+#opcao_escolhida = int(opcao_escolhida)
+#É, pode usar o mesmo nome de variável, mesmo.
+
+#OU:
+
+opcao_escolhida = int(input ('Escolha uma opção: '))
+
+if opcao_escolhida == 1:
+    print ('Cadastrar Restaurante')
+
+
+elif opcao_escolhida == 2:
+    print ('Listar Restaurantes')
+
+
+elif opcao_escolhida == 3:
+    print ('Ativar Restaurante')
+
+
+elif opcao_escolhida == 4:
+    print ('Encerrando o programa')
+
+else:
+    print ('São quatro opções, bundão. Escolhe uma e digite o número de 1 a 4. Não é difícil.')
